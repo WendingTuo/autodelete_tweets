682ce13790587ffccbadc6e73a095c29e659fade
WendingTuo <blake.hyatt@pm.me>
2021-06-08 21:57:52 -0400

    Finalized the delete function for tweets Need to add favorites functionality

--- autodelete_cc.py
+++ autodelete_cc.py
@@ -1,5 +1,7 @@
 # Import modules
 import os
+import sys
+import time
 import tweepy as tw
 from datetime import datetime, timedelta
 
@@ -14,7 +16,7 @@ user = api.me()
 user_id = user.id
 
 #* Set how far back you'd like to retain tweets
-daysAgo = 100 #!Commenting out to streamline testing #int(input('How far back would you like to retain your tweets?'))
+daysAgo = 110 #!Commenting out to streamline testing #int(input('How far back would you like to retain your tweets?'))
 
 oldestDateToKeep = datetime.now() - timedelta(days=daysAgo)
 
@@ -32,6 +34,32 @@ for tweet in rawTweets:
     if tweet.created_at < oldestDateToKeep and tweet.favorited is True:
         print(tweet.id)
         tweetsToUnfavorite.append(tweet.id)
-    
+
 print(tweetsToDelete)
 print(tweetsToUnfavorite)
+
+def deleteTweets(tweetsList):
+    for tweet in tweetsList:
+        try:
+            api.destroy_status(tweet)
+            print("Deleted:", tweet)
+        except Exception:
+            print("Failed to delete:", tweet)
+
+def unfavoriteTweets(tweetsList):
+    for tweet in tweetsList:
+        try:
+            api.destroy_favorite(tweet)
+            print("Deleted:", tweet)
+        except Exception:
+            print("Failed to delete:", tweet)
+
+while (res:= input('Do you want to delete ' + str(len(tweetsToDelete)) + ' and unfavorite ' + str(len(tweetsToUnfavorite)) + ' tweets? (y/n): ').lower()) not in {"y", "n"}: pass
+if res=='y':
+    print("Deleting/Unfavoriting tweets now!")
+    deleteTweets(tweetsToDelete)
+    unfavoriteTweets(tweetsToUnfavorite)
+if res=='n':
+    print("Exiting in 3 seconds...")
+    time.sleep(3)
+sys.exit()
